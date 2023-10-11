e2f5503d87c395fc09080420d729e820ccbeb1ee
Jonathan Kump <landon2002@gmail.com>
2023-10-11 13:09:53 -0500

    Build setup

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// Graphics Imports
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <stb_image.h>
+
+// Project Imports
+#include <helpers/RootDir.h>
+
+// GUI Imports
+#define NK_IMPLEMENTATION
+#include "nuklear.h"
+
+int main()
+{
+	printf("Hello!!\n");
+	getc(stdin);
+}
