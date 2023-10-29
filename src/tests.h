bbc702ec4dc493fc4059319112a74e5af8c40e5f
Jonathan Kump <landon2002@gmail.com>
2023-10-29 15:21:41 -0500

    More tests

--- src/tests.h
+++ src/tests.h
@@ -53,7 +53,7 @@ bool TestCyclicDependency() {
 	return false;
 }
 
-bool TestExpressionStructure() {
+bool TestExpressionStructure_TestOne() {
 	Expression e = Expression("temp");
 	e.SetExpression("8*2+1");
 	string structure;
@@ -64,35 +64,14 @@ bool TestExpressionStructure() {
 		return false;
 	}
 	return true;
-	//ExpressionNode* cur = e.ast_root_;
-	//if (cur->node_value_ != "+" || cur->type_ != ExpressionNode::ADDITION) {
-	//	return false;
-	//}
-	//for (ExpressionNode* child : cur->children_) {
-	//	if (child->type_ == ExpressionNode::TRUE_VALUE && child->node_value_ != "1") {
-	//		return false;
-	//	}
-	//	if (child->type_ == ExpressionNode::MULTIPLICATION && child->node_value_ != "*") {
-	//		return false;
-	//	}
-	//	else if (child->node_value_ == "*") {
-	//		if ((child->children_[0]->node_value_ != "8" && child->children[1] != "2") && (child->children_[0]->node_value_ != "2" && child->children[1] != "8")) {
-	//			return false;
-	//		}
-	//	}
-	//	if (child->type_ != ExpressionNode::TRUE_VALUE || child->type_ != ExpressionNode::MULTIPLICATION) {
-	//		return false;
-	//	}
-	//}
-	//return true;
 }
 
-bool TestExpressionStructure() {
+bool TestExpressionStructure_TestTwo() {
 	Expression e = Expression("temp");
-	e.SetExpression("8*2+1");
+	e.SetExpression("8*rounddown(2)+1");
 	string structure;
 	e.GetPrettyTreeString(structure);
-	string expected = "|__+\n    |__*\n    |   |__8\n    |   |__2\n    |__1\n";
+	string expected = "|__+\n    |__*\n    |   |__8\n    |   |__rounddown\n    |       |__2\n    |__1\n";
 	if (structure != expected) {
 		cout << "Got: \n" << structure << "Expected: \n" << expected;
 		return false;
@@ -103,7 +82,8 @@ bool TestExpressionStructure() {
 void RunTests() {
 	PrintTestResults("Uneven Parentheses", TestUnevenParentheses());
 	PrintTestResults("Cyclic Dependency", TestCyclicDependency());
-	PrintTestResults("Expression Structure", TestExpressionStructure());
+	PrintTestResults("Expression Structure Test 1", TestExpressionStructure_TestOne());
+	PrintTestResults("Expression Structure Test 2", TestExpressionStructure_TestTwo());
 }
 
 
